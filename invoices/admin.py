5118dca037dc192714bf9be3bdff45040c35df06
nimondo <znimed@gmail.com>
2024-09-03 23:44:29 +0000

    Add model for invoice

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Client, Category, Product
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Product)
--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    reference = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    tva = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
