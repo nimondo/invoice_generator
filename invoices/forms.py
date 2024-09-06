60b3ee736a090d8cf974e42e78d92fb78f18ec73
nimondo <znimed@gmail.com>
2024-09-06 08:59:45 +0000

    add english label

--- invoices/forms.py
+++ invoices/forms.py
@@ -5,6 +5,9 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['name']
+        labels = {
+            'name': 'Nom de la catégorie',
+        }
     def __init__(self, *args, **kwargs):
         super(CategoryForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
@@ -13,6 +16,15 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'brand', 'reference', 'category', 'description', 'price', 'tva']
+        labels = {
+            'name': 'Nom du produit',
+            'brand': 'Marque',
+            'reference': 'Numéro de référence',
+            'category': 'Catégorie',
+            'description': 'Description',
+            'price': 'Prix (€)',
+            'tva': 'TVA (%)',
+        }
 
     def __init__(self, *args, **kwargs):
         super(ProductForm, self).__init__(*args, **kwargs)
@@ -39,6 +51,16 @@ class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['sex','first_name', 'last_name', 'address', 'postal_code', 'city', 'email', 'phone']
+        labels = {
+            'first_name': 'Prénom',
+            'last_name': 'Nom',
+            'address': 'Adresse',
+            'postal_code': 'Code Postal',
+            'city': 'Ville',
+            'email': 'E-mail',
+            'phone': 'Téléphone',
+            'sex': 'Civilité',
+        }
     def __init__(self, *args, **kwargs):
         super(ClientForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
